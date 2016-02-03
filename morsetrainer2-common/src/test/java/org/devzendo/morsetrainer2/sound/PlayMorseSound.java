b77572e51252ab88ad44b6221dddd7cf97d1f5ad
Matt Gumbley <matt.gumbley@devzendo.org>
2016-02-03 08:02:16 +0000

    A better listener, for synchronous playback.

--- morsetrainer2-common/src/test/java/org/devzendo/morsetrainer2/sound/PlayMorseSound.java
+++ morsetrainer2-common/src/test/java/org/devzendo/morsetrainer2/sound/PlayMorseSound.java
@@ -1,68 +1,55 @@
 package org.devzendo.morsetrainer2.sound;
 
+import java.util.concurrent.CountDownLatch;
+
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineEvent.Type;
 import javax.sound.sampled.LineListener;
 
 import org.apache.log4j.BasicConfigurator;
-import org.devzendo.morsetrainer2.sound.PlaySilence.AudioListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PlayMorseSound {
 	private static Logger LOGGER = LoggerFactory.getLogger(PlayMorseSound.class);
 
-	public static void play(final Clip clip) throws InterruptedException {
+	public static void playSynchronously(final Clip clip) throws InterruptedException {
 		//LOGGER.debug("playing clip " + clip);
-		AudioListener listener = new AudioListener();
+		final CountDownLatch latch = new CountDownLatch(1);
+		
+		final LineListener listener = new LineListener() {
+			public void update(LineEvent event) {
+				Type eventType = event.getType();
+				//System.out.println("event " + eventType);
+				if (eventType == Type.STOP || eventType == Type.CLOSE) {
+					latch.countDown();;
+				}
+			}};
 		clip.addLineListener(listener);
 		clip.setFramePosition(0);
 
 		clip.start();
 		//clip.drain(); // drain works ish on 1.6 but seems to return too fast on 1.7/1.8
-		listener.waitUntilDone();
+		latch.await();
 		clip.removeLineListener(listener);
-	}
-
-	static class AudioListener implements LineListener {
-		private boolean done = false;
-
-		public synchronized void reset() {
-			done = false;
-		}
-
-		public synchronized void update(LineEvent event) {
-			Type eventType = event.getType();
-			//System.out.println("event " + eventType);
-			if (eventType == Type.STOP || eventType == Type.CLOSE) {
-				done = true;
-				notifyAll();
-			}
-		}
-
-		public synchronized void waitUntilDone() throws InterruptedException {
-			while (!done) {
-				wait();
-			}
-		}
 	}
 
 	public static void main(final String[] args) throws InterruptedException {
 		BasicConfigurator.configure();
 
 		LOGGER.info("Starting ClipGenerator");
-		final ClipGenerator clipGen = new ClipGenerator(12, 12, 600);
+		final ClipGenerator clipGen = new ClipGenerator(20, 20, 600);
 		LOGGER.info("Playing...");
 		for (int i = 0; i < 10; i++) {
-			play(clipGen.getDit());
-			play(clipGen.getElementSpace());
-			play(clipGen.getDah());
-			play(clipGen.getElementSpace());
-			play(clipGen.getDit());
-			play(clipGen.getElementSpace());
-			play(clipGen.getDit());
-			play(clipGen.getCharacterSpace());
+			playSynchronously(clipGen.getDit());
+			playSynchronously(clipGen.getElementSpace());
+			playSynchronously(clipGen.getDah());
+			playSynchronously(clipGen.getElementSpace());
+			playSynchronously(clipGen.getDit());
+			playSynchronously(clipGen.getElementSpace());
+			playSynchronously(clipGen.getDit());
+			playSynchronously(clipGen.getCharacterSpace());
 		}
 		LOGGER.info("Finished playing");
 	}
