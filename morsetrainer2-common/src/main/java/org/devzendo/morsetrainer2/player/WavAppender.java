0e6fd53211111b03ea8bf4f6dfd2a56e009bfa9e
Matt Gumbley <matt.gumbley@devzendo.org>
2016-03-10 08:43:12 +0000

    Commit the WavAppender

--- morsetrainer2-common/src/main/java/org/devzendo/morsetrainer2/player/WavAppender.java
+++ morsetrainer2-common/src/main/java/org/devzendo/morsetrainer2/player/WavAppender.java
@@ -5,12 +5,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioFormat.Encoding;
 
 public class WavAppender {
 	private final FileOutputStream fileOutputStream;
 
 	public WavAppender(final File outputFile, final AudioFormat format) throws IOException {
+		if (!format.getEncoding().equals(Encoding.PCM_SIGNED)) {
+			throw new IllegalArgumentException("Only PCM_SIGNED data supported at this time");
+		}
+
 		fileOutputStream = new FileOutputStream(outputFile);
+
 	}
 
 	public void append(final byte[] waveform) {
